289fe76eb0da688a2e4747c9ea8706eae2315b5c
RamejaAyush <Ayushrwork@gmail.com>
2022-07-11 17:27:22 +0530

    NavLinks styles Added

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,18 +3,22 @@ import "../Styles/Nav.scss";
 const Nav = () => {
   return (
     <nav>
-      <div className="left-links">
-        <a href="#">About</a>
-        <a href="#">Resume</a>
-      </div>
-
       <div className="logo">
         <p>AXU</p>
       </div>
-
-      <div className="right-links">
-        <a href="#">Projects</a>
-        <a href="#">Contact</a>
+      <div className="nav-links">
+        <a href="#">
+          <span data-hover="About">About</span>
+        </a>
+        <a href="#">
+          <span data-hover="Project">Projects</span>
+        </a>
+        <a href="#">
+          <span data-hover="Designs">Designs</span>
+        </a>
+        <a href="#">
+          <span data-hover="Contact">Contact</span>
+        </a>
       </div>
     </nav>
   );
