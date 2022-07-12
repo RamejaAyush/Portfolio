dc0573afd5c0d5acb45be4d18787779dd1908cda
RamejaAyush <ayushrwork@gmail.com>
2022-07-12 18:00:26 +0530

    Added Loader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import Home from "./Components/Home";
+import Loader from "./Components/Loader";
 import Nav from "./Components/Nav";
 
 const App = () => {
+  const [isLoading, setisLoading] = useState(true);
   return (
     <div className="App">
-      <>
-        <Nav />
-        <Home />
-      </>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Nav />
+          <Home />
+        </>
+      )}
     </div>
   );
 };
