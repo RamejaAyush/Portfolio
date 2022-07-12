3e0f271dc2f1117fae970f5939d411eb2aaa9d36
RamejaAyush <ayushrwork@gmail.com>
2022-07-12 12:05:43 +0530

    Added side-info-links

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,13 +5,25 @@ import { BsGithub } from "react-icons/bs";
 
 const Home = () => {
   return (
-    <div className="Home-container">
+    <div className="home-container">
       <div className="left-side-info">
-        <BsLinkedin />
-        <BsTwitter />
-        <BsGithub />
+        <div className="line"></div>
+        <div className="links">
+          <a href="#">
+            <BsLinkedin className="link" />
+          </a>
+          <a href="#">
+            <BsTwitter className="link" />
+          </a>
+          <a href="#">
+            <BsGithub className="link" />
+          </a>
+        </div>
       </div>
-      <div className="right-side-info"></div>
+      <div className="right-side-info">
+        <div className="line"></div>
+        <a href="mailto:ayushrameja@gmail.com">ayushrameja@gmail.com</a>
+      </div>
     </div>
   );
 };
